128890be53438ce2abed11a83eaa409f393885f4
mahimakumar3 <80737223+mahimakumar3@users.noreply.github.com>
2021-04-30 23:32:24 +0530

    Update activity4.h

--- Implementation/inc/activity4.h
+++ Implementation/inc/activity4.h
@@ -3,16 +3,9 @@
 #include<avr/io.h>
 
 void InitUART(uint16_t ubrr);
-/**
- * @brief Read characters from UART port
- * @return char
- */
+
 char UARTread();
-/**
- * @brief Write characters to UART port
- *
- * @param data
- */
+
 void UARTwrite(char data);
 
 
